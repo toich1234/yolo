343fa8d7e725530efd1f40c669ee0bc27cd27f11
toich1234 <82895167+toich1234@users.noreply.github.com>
2022-05-20 19:16:42 +0900

    Update detect.py

--- detect.py
+++ detect.py
@@ -240,7 +240,7 @@ def run(
                         
                        
                    
-                        data_dir = C:\Users\toich\yolo\runs\detect\exp\crops\f'{pic}.jpg'
+                        data_dir = r"C:\Users\toich\yolo\runs\detect\exp\crops\f'{pic}.jpg'"
                         #image_files = []
                         #for file in os.listdir(data_dir):
                         #    image_files.append(os.path.join(data_dir, file))
