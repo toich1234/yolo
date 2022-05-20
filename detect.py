94dba560d5f7342676e14fb413e802d5dce5a652
toich1234 <82895167+toich1234@users.noreply.github.com>
2022-05-20 19:56:55 +0900

    Update detect.py

--- detect.py
+++ detect.py
@@ -50,10 +50,10 @@ class efficientnet_b2(nn.Module): #Resnet50
         return self.fc(x)
 
 save_dir1=r'E:\KyungHee\K-Fashion'
-model = efficientnet_b2(num_classes=4)
+model1 = efficientnet_b2(num_classes=4)
 device = torch.device('cpu')
-model.load_state_dict(torch.load(f"{save_dir1}/best.pth", map_location=device))
-model.eval()
+model1.load_state_dict(torch.load(f"{save_dir1}/best.pth", map_location=device))
+model1.eval()
 
 
 import argparse
@@ -248,7 +248,7 @@ def run(
                             img = transforms.ToTensor()(img)
                             img = transforms.Normalize((0.1307,),(0.3081,))(img)
                             img = transforms.Resize((200,200))(img) #,Image.BILINEAR
-                            out = model(img)
+                            out = model1(img)
                             label1 = torch.argmax(out, dim=-1)
                             print(label1)
                     #data_dir = save_dir / 'crops' / f'{pic}.jpg'
